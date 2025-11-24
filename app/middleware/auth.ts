f37246674da0533845183a88bf9cbb53d0eda91a
Clément Charbonnel <c.charbo38@gmail.com>
2025-11-24 23:10:20 +0100

    fix(auth): allow ssr to access user auth token
    Before that the authentication was done only in client side, so refreshes on '/' cause the page to blink on '/login'.

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,6 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   try {
-    await $fetch('/api/users/me')
+    if (import.meta.server) {
+      const headers = useRequestHeaders(['cookie'])
+      await $fetch('/api/users/me', { headers })
+    } else {
+      await $fetch('/api/users/me')
+    }
 
     if (to.path === '/login' || to.path === '/register') {
       return navigateTo('/')
