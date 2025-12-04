acbc94aa00abb8c11d736d273e1e811e57956105
Clément Charbonnel <c.charbo38@gmail.com>
2025-12-04 23:00:42 +0100

    feat(error): add custom error page component
    Implement a new error page to display status code and message, enhancing user experience during errors.

---
+++ app/error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{
+  error: NuxtError
+}>()
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <main
+    class="flex h-dvh w-full flex-col items-center justify-center px-4 text-center"
+  >
+    <h1 class="mb-4 text-8xl font-bold">{{ props.error.statusCode }}</h1>
+    <p class="mb-2 text-2xl">Oops, {{ props.error.message }}!</p>
+    <p class="text-text-muted mb-8 text-sm">{{ props.error.data?.detail }}</p>
+    <BaseButton @click="handleError"> Go back to Home </BaseButton>
+  </main>
+</template>
+
+<style scoped></style>
